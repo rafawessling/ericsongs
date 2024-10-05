a366577fbc8631403c114552850ecdcbc766977c
rafawessling <rafaoening@gmail.com>
2024-10-05 16:25:28 +0200

    Added prop type to Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,14 @@
 interface ButtonProps {
     children: string;
+    type: 'button' | 'submit';
 }
 
-export const Button = ({ children }: ButtonProps) => {
+export const Button = ({ children, type }: ButtonProps) => {
     return (
-        <button className="py-2.5 px-4 font-light rounded-3xl text-zinc-50 bg-blue-500 hover:bg-blue-600">
+        <button
+            type={type}
+            className="py-2.5 px-4 font-light rounded-3xl text-zinc-50 bg-blue-500 hover:bg-blue-600"
+        >
             {children}
         </button>
     );
