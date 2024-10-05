762b06e7caf8d6da87b6ea0bf111429380e370b2
rafawessling <rafaoening@gmail.com>
2024-10-05 21:54:43 +0200

    Adapted SignIn page for smaller devices and added props to Form component

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -3,31 +3,58 @@ import { Button } from '../Button/Button';
 import { Input } from '../Input/Input';
 import { Title } from '../Title/Title';
 
-export const Form = () => {
-    const [showPassword, setShowPassword] = useState(false);
+interface FormProps {
+    type: string;
+    title: string;
+    subtitle: string;
+    className?: string;
+}
 
-    const handlePassword = () => {
-        setShowPassword(!showPassword);
+export const Form = ({ type, title, subtitle, className }: FormProps) => {
+    const [showPassword, setShowPassword] = useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+    const isSignUp = type === 'signup';
+
+    const handlePassword = (
+        setPassword: React.Dispatch<React.SetStateAction<boolean>>
+    ) => {
+        return () => setPassword(prevState => !prevState);
     };
 
     return (
-        <form className="flex flex-col justify-start align-center gap-10 md:gap-12 w-full md:w-4/5  p-6 m-8">
+        <form
+            className={`flex flex-col justify-start align-center gap-10 md:gap-12 z-20 w-full md:w-4/5 min-[390px]:py-8 ${className}`}
+        >
             <div className="flex flex-col gap-2">
-                <Title>Sign In</Title>
-                <p className="text-lg">Enter yout credentials to access your account.</p>
+                <Title>{title}</Title>
+                <p className="text-lg">{subtitle}</p>
             </div>
-            <div className="flex flex-col gap-6 md:gap-8 w-full">
+            <div
+                className={`flex flex-col gap-6 md:gap-8 w-full ${
+                    isSignUp ? 'lg:gap-4 xl:gap-6' : ''
+                }`}
+            >
+                {isSignUp && <Input id="name" placeholder="Name" type="text" />}
                 <Input id="email" placeholder="Email" type="email" />
                 <Input
                     id="password"
                     placeholder="Password"
                     type={showPassword ? 'text' : 'password'}
                     endAdornment={showPassword ? 'EyeOff' : 'Eye'}
-                    onIconClick={handlePassword}
+                    onIconClick={handlePassword(setShowPassword)}
                 />
+                {isSignUp && (
+                    <Input
+                        id="confirmPassword"
+                        placeholder="Confirm password"
+                        type={showConfirmPassword ? 'text' : 'password'}
+                        endAdornment={showConfirmPassword ? 'EyeOff' : 'Eye'}
+                        onIconClick={handlePassword(setShowConfirmPassword)}
+                    />
+                )}
             </div>
-            <Button type="submit">Sign In</Button>
-            <p className="text-center mt-5">
+            <Button type="submit">Sign Up</Button>
+            <p className="text-center">
                 Do not have an account?{' '}
                 <span className="py-3 px-1 cursor-pointer text-blue-500 underline">
                     Sign up
--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -7,28 +7,32 @@ export const SignIn = () => {
         <main className="flex flex-col lg:flex-row-reverse items-center w-screen min-h-dvh text-zinc-50 bg-primary">
             <section className="flex flex-col items-center gap-16 w-full relative lg:w-3/5 lg:h-screen lg:bg-secondary lg:rounded-l-[80px]">
                 <img
-                    className="z-0 absolute t-0 w-full min-h-52 max-h-56 object-cover md:max-h-64 lg:max-h-[340px] 2xl:max-h-[400px] lg:rounded-tl-[80px]"
+                    className="z-0 absolute t-0 w-full h-40 min-[390px]:min-h-52 max-h-56 object-cover md:max-h-64 lg:max-h-[340px] 2xl:max-h-[400px] lg:rounded-tl-[80px]"
                     src={background}
                     alt="Background"
                 />
-                <div className="flex justify-center items-end z-10 w-full h-32 mt-32 md:h-48 md:mt-40 lg:h-64 lg:mt-64 2xl:h-80 2xl:mt-80 bg-gradient-signin-mobile lg:bg-gradient-signin-desktop">
+                <div className="flex justify-center items-end z-10 w-full h-32 mt-20 min-[390px]:mt-32 md:h-48 md:mt-40 lg:h-64 lg:mt-64 2xl:h-80 2xl:mt-80 bg-gradient-signin-mobile lg:bg-gradient-signin-desktop">
                     <h3 className="hidden lg:block text-4xl leading-normal text-center w-[500px] lg:mt-20">
                         Discover and search your favorite songs and artists with
                     </h3>
                 </div>
                 <img
-                    className="z-20 h-8 -mt-16 sm:h-10 md:h-12 md:-mt-24 lg:mt-0 lg:h-14"
+                    className="z-20 h-8 -mt-28 min-[390px]:-mt-16 sm:h-10 md:h-12 md:-mt-24 lg:mt-0 lg:h-14"
                     src={logo}
                     alt="Ericsongs logo"
                 />
             </section>
-            <section className="flex flex-col justify-center items-center w-full relative lg:w-2/5 lg:h-screen">
+            <section className="flex flex-col justify-center items-center p-6 w-full relative lg:w-2/5 lg:h-screen">
                 <img
                     className="hidden lg:block absolute h-8 top-8 left-8"
                     src={logo}
                     alt="Ericsongs logo"
                 />
-                <Form />
+                <Form
+                    type="signin"
+                    title="Sign In"
+                    subtitle="Enter your credentials to access your account."
+                />
             </section>
         </main>
     );
