6f17ab58470035d1eb85fe819d0deaecbff83545
rafawessling <rafaoening@gmail.com>
2024-10-06 19:54:40 +0200

    Added startAdornment prop to Input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -3,21 +3,38 @@ import { ComponentProps } from 'react';
 import { inputIcons } from '../../utils/inputIcons';
 
 interface InputProps extends ComponentProps<'input'> {
+    startAdornment?: keyof typeof inputIcons;
     endAdornment?: keyof typeof inputIcons;
     onIconClick?: () => void;
+    className?: string;
 }
 
-export const Input = ({ endAdornment, onIconClick, ...props }: InputProps) => {
+export const Input = ({
+    startAdornment,
+    endAdornment,
+    onIconClick,
+    className,
+    ...props
+}: InputProps) => {
+    const StartAdornmentIcon = startAdornment ? inputIcons[startAdornment] : null;
     const EndAdornmentIcon = endAdornment ? inputIcons[endAdornment] : null;
 
     return (
-        <div className="card flex flex-col justify-content-center w-full p-float-label">
+        <div
+            className={`${className} card flex flex-col justify-content-center w-full p-float-label`}
+        >
+            {StartAdornmentIcon && (
+                <StartAdornmentIcon
+                    onClick={onIconClick}
+                    className="absolute left-4 top-1/4 size-5 text-zinc-200"
+                />
+            )}
             <InputText
                 {...props}
                 id={props.id}
                 value={props.value?.toString()}
                 placeholder={props.placeholder}
-                className={`${
+                className={`${startAdornment ? 'pl-12' : ''} ${
                     endAdornment ? 'pr-12' : ''
                 } w-full py-2.5 px-4 font-light rounded-3xl text-zinc-50 bg-gray-800 placeholder:text-zinc-300 focus:outline-none focus:ring-1 focus:ring-gray-700`}
             />
--- src/utils/inputIcons.ts
+++ src/utils/inputIcons.ts
@@ -1,6 +1,8 @@
-import { Eye, EyeOff } from 'lucide-react';
+import { Eye, EyeOff, Search, X } from 'lucide-react';
 
 export const inputIcons = {
     Eye: Eye,
     EyeOff: EyeOff,
+    Search: Search,
+    X: X,
 };
